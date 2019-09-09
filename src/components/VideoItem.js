09854d995b25dfee8a17c986fecee01b17b11550
Jean Rauwers <jeanrauwers@gmail.com>
2019-09-09 09:58:50 +0100

    Add Img

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,6 +5,7 @@ export const VideoItem = ({ video }) => {
 
     return (
         <div className="search-bar ui segment">
+            <img src={video.snippet.thumbnails.medium.url}/>
             Video Title : {video.snippet.title}
             Video Description : {video.snippet.description}
         </div>
