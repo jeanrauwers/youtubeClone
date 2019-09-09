a2e4d3f5084c6e865de6da4a765ef01a50e17a96
Jean Rauwers <jeanrauwers@gmail.com>
2019-09-09 10:09:11 +0100

    Add style to list

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,13 +1,15 @@
 
 import React from 'react'
-
+import './VideoItem.css'
 export const VideoItem = ({ video }) => {
 
     return (
-        <div className="search-bar ui segment">
-            <img src={video.snippet.thumbnails.medium.url}/>
-            Video Title : {video.snippet.title}
-            Video Description : {video.snippet.description}
+        <div className="video-item item">
+            <img className="ui image"src={video.snippet.thumbnails.medium.url}/>
+            <div className="content">
+            <div className="header">{video.snippet.title}</div>
+            {/* <div className="description">{video.snippet.description}</div> */}
+            </div>
         </div>
     );
 }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,10 +2,7 @@
 import React from 'react'
 import { VideoItem } from './VideoItem'
 export const VideoList = ({ videos }) => {
+    const renderedList = videos.map((video, index) => <VideoItem video={video} key={index} />)
 
-    return (
-        <div className="">
-            {videos ? videos.map((video, index) => <VideoItem video={video} key={index} />) : ""}
-        </div>
-    );
+    return <div className="ui relaxed divided list">{renderedList}</div>
 } 
