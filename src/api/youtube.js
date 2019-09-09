ab69aa5f665784221f80163d2050fe6078f83465
Jean Rauwers <jeanrauwers@gmail.com>
2019-09-09 09:20:32 +0100

    removing typo on Youtube config

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const KEY = 'AIzaSyCKfILu3AbAhbIxzwz5BNg5HxLoIDvn8B0';
 
 export default axios.create({
-    baseURL = 'https://www.googleapis.com/youtube/v3',
+    baseURL:'https://www.googleapis.com/youtube/v3',
     params:{
         part:'snippet',
         maxResults:6,
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import youtube from '../api/youtube'
 import { SearchBar } from './SearchBar'
 export default class App extends React.Component {
     onSearchSubmit = (searchTerm) => {
