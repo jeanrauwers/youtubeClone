55cc06a61969ae42d55186e6d1d0d910063f708d
Jean Rauwers <jeanrauwers@gmail.com>
2019-09-09 09:46:49 +0100

    Videos state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import youtube, { KEY } from '../api/youtube'
 import { SearchBar } from './SearchBar'
 
 export const App = () => {
-    const onSearchSubmit = (searchTerm) => {
-        return youtube.get('/search', {
+    const [videos, setVideos] = useState([])
+
+
+    const onSearchSubmit = async (searchTerm) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: searchTerm,
                 part: 'snippet',
@@ -12,11 +15,15 @@ export const App = () => {
                 key: KEY
             }
         })
-    }
+        setVideos(response.data.items)
 
+    }
+    console.log(videos)
     return (
+
         <div className="ui container">
             <SearchBar onSearchSubmit={onSearchSubmit} />
+            I have {videos.length} videos
         </div>
     );
 
