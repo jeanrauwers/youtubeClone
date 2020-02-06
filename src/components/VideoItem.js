70cd1ee18befc1e236b8e6b802d069445d552922
Jean Rauwers <jeanrauwers@gmail.com>
2020-02-06 18:48:08 +0000

    Add back the description

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,10 +5,10 @@ export const VideoItem = ({ video, index, setSelectedVideo }) => {
 
     return (
         <div className="video-item item" onClick={() => setSelectedVideo(video)}>
-            <img className="ui image"src={video.snippet.thumbnails.medium.url}/>
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
-            <div className="header">{video.snippet.title}</div>
-            {/* <div className="description">{video.snippet.description}</div> */}
+                <div className="header">{video.snippet.title}</div>
+                <div className="description">{video.snippet.description}</div>
             </div>
         </div>
     );
